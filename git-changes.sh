2b7cad7d3f51616c34ddb73bda917ad53c4e8ceb
Mikhail Surin <surinmike@gmail.com>
2019-05-19 21:01:36 +0300

    scripts

--- git-changes.sh
+++ git-changes.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+p=$PWD
 cd $1
-git status --porcelain | cut -d' ' -f2
+git status --porcelain | sed -e 's/.*\s//g' > $p/.changelist
+cd $p
+python daemon.py --src $1 --host $2 --dst $3 --sync .changelist
--- svn-changes.sh
+++ svn-changes.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-svn st -q $1 | cut -d ' ' -f 8
+svn st $1 | cut -d ' ' -f 8 > .changelist
+python daemon.py --src $1 --host $2 --dst $3 --sync .changelist
