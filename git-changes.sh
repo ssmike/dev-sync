78889bffb584d772bf17b4590b11be6ecdf81596
Mikhail Surin <surinmike@gmail.com>
2019-06-23 15:46:09 +0300

    don't exec daemon from change-scripts

--- git-changes.sh
+++ git-changes.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-p=$PWD
 cd $1
-git status --porcelain | sed -e 's/.*\s//g' > $p/.changelist
-cd $p
-DIR=`dirname $0`
-python $DIR/daemon.py --src $1 --host $2 --dst $3 --sync .changelist
+git status --porcelain | sed -e 's/.*\s//g'
--- svn-changes.sh
+++ svn-changes.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-svn st $1 | sed -e 's/.*\s//g' > .changelist
-DIR=`dirname $0`
-python $DIR/daemon.py --src $1 --host $2 --dst $3 --sync .changelist
+svn st $1 | sed -e 's/.*\s//g'
